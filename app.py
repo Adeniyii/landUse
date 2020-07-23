2837f686e06d0729538cb6243b5a7eddcdb9355c
Ifedeniyi <ifedayoadeniyi@gmail.com>
2020-07-23 18:10:26 +0100

    Added Login and register endpoints and views

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 load_dotenv()
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
-
 
 posts = [
     {
@@ -23,16 +23,42 @@ posts = [
     }
 ]
 
-
+# this will be the welcome page
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("home.html", posts=posts)
 
+
 @app.route("/about")
 def about():
     return render_template("about.html", posts=posts, title="About")
 
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for("dashboard"))
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "l@g.co" and form.password.data == "111":
+            flash(f"Successfully logged in {form.email.data}!", "success")
+            return redirect(url_for("dashboard"))
+        else:
+            flash(f"Log in Unuccessful. Please try again.", "danger")
+    return render_template("login.html", title="Login", form=form)
+
+
+@app.route("/dashboard", methods=["GET", "POST"])
+def dashboard():
+    return render_template("dashboard.html", title="Dashboard")
 
 # modules are named "__main__" when run directly with python
 if __name__ == '__main__':
